47015129b06ba7f91ae8315c4e0f848f75974ac1
Pewpews <freaker12@hotmail.com>
2015-05-01 23:52:33 +0200

    fixed conflict
    Former-commit-id: 4bc204b69300732f30fad0a4f2b3e76190341f0c
    Former-commit-id: a5b2f63da3af4c8941c37a8198c8993fdb0b81f2
    Former-commit-id: 77c3b77f639a55a8086c49c83c87dd012c698bc7

--- version/gui/gui_constants.py
+++ version/gui/gui_constants.py
@@ -1,4 +1,6 @@
-# default stylesheet path
+"""Contains constants to be used by several modules"""
+
+#default stylesheet path
 default_stylesheet_path = ""
 user_stylesheet_path = ""
 PIXMAP_PATH = "" # PATH TO IMAGE HERE
