67c3a94d8f0604c9908d4bab13f8aad5d9472ab8
LePearlo <vara.sahkpost2@gmail.com>
2015-05-02 00:00:05 +0300

    Update db_constants.py
    Former-commit-id: 1f08d52ff42e978e22c97629cb66952ec9e5ffd7
    Former-commit-id: 8d8c724532f7fd91ee949aa4031728cca3d689fe
    Former-commit-id: 042878aa64262dbf1cddfec3e0db943e7e81b3d5

--- version/database/db_constants.py
+++ version/database/db_constants.py
@@ -1,6 +1,6 @@
 DB_PATH = ""
 
-SERIES_PATH = "/home/earlo/Desktop/Py_Projects/Sad_Pand/Series" #your folder for series here
+SERIES_PATH = "" #your folder for series here
 
 IMG_FILES = ['jpg','bmp','png','gif'] 
 
