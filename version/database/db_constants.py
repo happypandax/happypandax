8476da859e6a6987fb7c9998a2f336112bb5284a
Pewpews <freaker12@hotmail.com>
2015-05-14 15:38:02 +0200

    updated to v0.4;
    Former-commit-id: 6d7e913ee01dc36eac5f2ad0f3535851c5edada4
    Former-commit-id: 458ad06a892f412182a6f63ddac8e4bee4bb519c
    Former-commit-id: 1db4496bd22cd098441b5bc147505630f5a371b6

--- version/database/db_constants.py
+++ version/database/db_constants.py
@@ -4,5 +4,5 @@ SERIES_PATH = "" #your folder for series here
 IMG_FILES = ['jpg','bmp','png','gif']
 
 DB_PATH = os.path.join("db","sadpanda.db")
-DB_VERSION = [0.3] # a list of accepted db versions. E.g. v3.5 will be backward compatible with v3.1 etc.
+DB_VERSION = [0.4] # a list of accepted db versions. E.g. v3.5 will be backward compatible with v3.1 etc.
 CURRENT_DB_VERSION = DB_VERSION[0]
