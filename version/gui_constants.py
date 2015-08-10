e8931025d6b73f26334bc217c78dead2d0d4757c
utterbull <utterbull@users.noreply.github.com>
2015-08-10 16:06:28 -0400

    Directory Fix

--- version/gui_constants.py
+++ version/gui_constants.py
@@ -21,8 +21,12 @@ DEBUG = False
 get = settings.get
 
 #current_dir, f = os.path.split(os.path.realpath(__file__))
-static_dir = os.path.join(os.getcwd(), "../res")
-temp_dir = os.path.join('../temp')
+if os.name == 'posix':
+	static_dir = os.path.join(os.getcwd(), "../res")
+	temp_dir = os.path.join('../temp')
+else:
+	static_dir = os.path.join(os.getcwd(), "res")
+	temp_dir = os.path.join('temp')
 
 #default stylesheet path
 default_stylesheet_path = static_dir + '/' + "style.css"
