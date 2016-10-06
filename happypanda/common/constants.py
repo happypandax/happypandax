abc40dc06f644808491dd60bfcb3d3c1316f6965
Pewpews <pew@pewpew.moe>
2016-10-06 20:50:49 +0200

    missing files;

---
+++ happypanda/common/constants.py
@@ -0,0 +1,10 @@
+﻿import os
+
+version = "0.0.1"
+
+## PATHS & FILENAMES ##
+
+log_dir = "logs"
+error_log = os.path.join(log_dir, "error.log")
+normal_log = os.path.join(log_dir, "activity.log")
+debug_log = os.path.join(log_dir, "debug.log")
