b4f2d6974b136dd1b1be19657c2c65305ce8f329
Pewpews <pew@pewpew.moe>
2016-10-06 20:48:52 +0200

    setup logging;

---
+++ run.py
@@ -0,0 +1,10 @@
+﻿from sys import exit
+from os import chdir, path
+
+# Change current working directory. This makes it so run.py is required to be on the same level as the happypanda folder
+chdir(path.abspath(path.dirname(__file__)))
+
+if __name__ == '__main__':
+    from happypanda import app
+
+    exit(app.exec_())
