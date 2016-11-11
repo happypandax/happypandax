ab2df3289ada96d4671a54b32072e7ab589ffb61
rachmadaniHaryono <foreturiga@gmail.com>
2016-11-11 10:07:38 +0800

    update test
    - comment the code for testing.
    - create mock up happyweb module.
    - fix TypeError when inititate parent class.
    - pass flake8.
    - revise test server.

--- happypanda/clients/web/__init__.py
+++ happypanda/clients/web/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask
+"""init module."""
+# NOTE unused
+# import os
+# from happypanda.clients.web import views
 
-import os
-
-from happypanda.common import utils
-
-happyweb = Flask(__name__, static_url_path='/static')
-client = utils.Client("webclient")
-
-from happypanda.clients.web import views
+# NOTE commented for testing.
+# from flask import Flask
+#
+# from happypanda.common import utils
+#
+# happyweb = Flask(__name__, static_url_path='/static')
+# client = utils.Client("webclient")
--- happypanda/clients/web/views.py
+++ happypanda/clients/web/views.py
@@ -1,14 +1,25 @@
-﻿from flask import render_template, flash, request, url_for, redirect, abort, g
+﻿"""views module."""
+from flask import (
+    render_template,
+    # NOTE unused
+    # flash,
+    # request,
+    # url_for,
+    # redirect,
+    # abort,
+    # g
+)
 from happypanda.clients.web import happyweb
 
+
 @happyweb.before_request
-def beforeRequest():
+def before_request():
+    """before request func."""
     pass
-    
+
 
 @happyweb.route('/')
 @happyweb.route('/index')
 def index():
+    """index func."""
     return render_template('index.html')
-
-
