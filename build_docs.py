ea52083a5bf864705ce8b9433714f419a6b6bd32
Twiddly <pew@pewpew.moe>
2017-03-11 20:05:30 +0100

    fixed build_docs.py;

--- build_docs.py
+++ build_docs.py
@@ -1,12 +1,21 @@
 import os
-from shutil import move
+import shutil
 from subprocess import run
 
 run(["sphinx-build", "-b", "html", "docs/source", "docs/build"])
 
-if os.path.exists("docs/build"):
-    b_dir = list(os.scandir("docs/build"))
-    for x in b_dir:
-        move(os.path.join("docs", "build", x.name), os.path.join("docs", x.name))
+root_src_dir = "docs/build"
+root_dst_dir = "docs/"
 
-    os.rmdir("docs/build")
+for src_dir, dirs, files in os.walk(root_src_dir):
+    dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    for file_ in files:
+        src_file = os.path.join(src_dir, file_)
+        dst_file = os.path.join(dst_dir, file_)
+        if os.path.exists(dst_file):
+            os.remove(dst_file)
+        shutil.move(src_file, dst_dir)
+
+shutil.rmtree(root_src_dir)
