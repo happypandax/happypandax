a21e923e1dc4bc3cd3c4a6e924c527b36b14693d
Twiddly <pew@pewpew.moe>
2017-03-13 11:54:35 +0100

    allow plugins to receive additional args;

--- plugins/example/hplugin.py
+++ plugins/example/hplugin.py
@@ -1,7 +1,12 @@
 class HPlugin:
-	ID = "9fb4ce2a-fe47-42ef-8c58-e039d999eb19"
-	NAME = "MyPlugin"
-	AUTHOR = "Pewpew"
-	DESCRIPTION = "MyPlugin makes you a happy panda!"
-	VERSION = (1, 0, 0)
-	WEBSITE = "https://github.com/Pewpews/happypandax"
+    ID = "9fb4ce2a-fe47-42ef-8c58-e039d999eb19"
+    NAME = "MyPlugin"
+    AUTHOR = "Pewpew"
+    DESCRIPTION = "MyPlugin makes you a happy panda!"
+    VERSION = (1, 0, 0)
+    WEBSITE = "https://github.com/Pewpews/happypandax"
+
+    def __init__(self, *args, **kwargs):
+        print("args:",args)
+        print("kwargs:",kwargs)
+
