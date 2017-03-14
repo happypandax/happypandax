5534c5d1f404cdfd672df2bc2d9a923d9a5a1fca
Twiddly <pew@pewpew.moe>
2017-03-14 19:44:36 +0100

    load core plugin;

--- happypanda/main.py
+++ happypanda/main.py
@@ -6,13 +6,14 @@ if __package__ is None and not hasattr(sys, 'frozen'):
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 from happypanda.common import utils, constants
-from happypanda.server.core import server
+from happypanda.server.core import server, plugins
 
 def start():
     parser = utils.get_argparser()
     args = parser.parse_args()
     utils.parse_options(args)
 
+    constants.core_plugin = plugins._plugin_load("happypanda.server.core.coreplugin", "core")
     server.HPServer().run(web=args.web, interactive=args.interact)
 
 if __name__ == '__main__':
