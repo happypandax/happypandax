2e9434c0d88493903372936a5ec2b2c46a41b432
Twiddly <pew@pewpew.moe>
2017-05-27 21:25:50 +0200

    added lint;

---
+++ lint.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from subprocess import run
+
+
+if __name__ == '__main__':
+
+    if not os.path.isdir("happypanda"):
+        print("This file needs to be run in the same directory as the 'happypanda/' directory")
+        sys.exit()
+
+    for root, dirs, files in os.walk("happypanda"):
+
+        for f in files:
+            if f.endswith('.py'):
+                f_path = os.path.join(root, f)
+                print("Auto formatting", f_path)
+                run(["autopep8", "--in-place", "-a", f_path])
+
+    print("Running flake8...\n")
+    run(["flake8", "--config", "setup.cfg", "happypanda"])
