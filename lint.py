0b6c4e4ff62aff53fb2431b50ba15284f861f4f9
Twiddly <pew@pewpew.moe>
2017-06-02 00:30:39 +0200

    add --skip-format cmd arg;

--- lint.py
+++ lint.py
@@ -1,19 +1,26 @@
 import os
 import sys
+import argparse
 from subprocess import run
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--skip-format', action='store_true', help="Skip autoformat")
+    args = parser.parse_args()
+
     if not os.path.isdir("happypanda"):
         print("This file needs to be run in the same directory as the 'happypanda/' directory")
         sys.exit()
 
-    for root, dirs, files in os.walk("happypanda"):
+    if not args.skip_format:
+        for root, dirs, files in os.walk("happypanda"):
 
-        for f in files:
-            if f.endswith('.py'):
-                f_path = os.path.join(root, f)
-                print("Auto formatting", f_path)
-                run(["autopep8", "--in-place", "-a", f_path])
+            for f in files:
+                if f.endswith('.py'):
+                    f_path = os.path.join(root, f)
+                    print("Auto formatting", f_path)
+                    run(["autopep8", "--in-place", "-a", f_path])
 
     print("Running flake8...\n")
     return run(["flake8", "--config", "./setup.cfg", "happypanda"]).returncode
