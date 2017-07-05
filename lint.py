277ad0e5e503dac7f31d8b39331b6c59aa64dbbe
Twiddly <pew@pewpew.moe>
2017-07-05 15:08:42 +0200

    bumped max line length;

--- lint.py
+++ lint.py
@@ -20,7 +20,7 @@ def main():
                 if f.endswith('.py'):
                     f_path = os.path.join(root, f)
                     print("Auto formatting", f_path)
-                    run(["autopep8", "--in-place", "-a", f_path])
+                    run(["autopep8", "--in-place", "-a", "--max-line-length=120", f_path])
 
     print("Running flake8...\n")
     return run(["flake8", "--config", "./setup.cfg", "happypanda"]).returncode
