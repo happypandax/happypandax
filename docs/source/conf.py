e6df42571e3d194fb3bb20554450ae336f072700
Twiddly <pew@pewpew.moe>
2017-07-20 16:18:40 +0200

    added exceptions;

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,6 @@ todo_link_only = True
 
 add_module_names = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
