e028fe17e87aa9882e04bcf6853f83172262ce2a
Twiddly <pew@pewpew.moe>
2023-04-29 15:48:31 +0200

    ench: improve pixie communication; ench: improve thumbnail fetching;

--- packages/client/services/pixie.ts
+++ packages/client/services/pixie.ts
@@ -1,5 +1,7 @@
+import EventEmitter from 'events';
+import { TimeoutError } from 'happypandax-client';
 import PQueue from 'p-queue';
-import { Request } from 'zeromq';
+import { Dealer } from 'zeromq';
 
 import { Decoder, Encoder } from '@msgpack/msgpack';
 
@@ -7,7 +9,6 @@ import { PIXIE_ENDPOINT, ServiceType } from '../server/constants';
 import { Service } from './base';
 
 import type { Endpoint } from './server';
-
 export async function getPixie() {
   const pixie = global.app.service.get(ServiceType.Pixie);
   if (!pixie.connected) {
@@ -45,29 +46,57 @@ type T = {
 };
 
 export default class PixieService extends Service {
+
+  TIMEOUT = 1000 * 30 // 30 sec
+
   #endpoint: string;
   #server_endpoint: Endpoint;
 
-  #queue: PQueue;
+  #emitter: EventEmitter;
+
+  #recv_queue: PQueue;
+
+  #queues: PQueue[];
+  #sockets: Dealer[];
+
   #decoder: Decoder;
   #encoder: Encoder;
-  #socket: Request;
   #connected: boolean;
 
   #session: string = '';
 
-  constructor(endpoint?: string) {
+  constructor(endpoint?: string, connections = 1) {
     super(ServiceType.Pixie);
     this.#connected = false;
-    this.#queue = new PQueue({ concurrency: 1 });
     this.#endpoint = endpoint;
+
+    this.#emitter = new EventEmitter();
+
     this.#encoder = new Encoder();
     this.#decoder = new Decoder();
-    this.#socket = new Request();
-    this.#socket.sendHighWaterMark = 1000;
-    this.#socket.connectTimeout = 1000 * 5; // 5 sec
-    this.#socket.sendTimeout = 1000 * 5; // 5 sec
-    this.#socket.receiveTimeout = 1000 * 30; // 30 secs
+
+    this.#recv_queue = new PQueue({ concurrency: connections });
+
+    this.#queues = [];
+    this.#sockets = [];
+
+    for (let i = 0; i < connections; i++) {
+
+      this.#queues.push(new PQueue({ concurrency: 1 }))
+
+      const s = new Dealer();
+      s.sendHighWaterMark = 1000;
+      s.connectTimeout = 1000 * 5; // 5 sec
+      s.sendTimeout = 1000 * 5; // 5 sec
+      s.receiveTimeout = -1; // forever
+
+      this.#sockets.push(s);
+
+    }
+
+    this.#sockets.forEach((s) => {
+      this.#recv_queue.add(() => this._recv(s));
+    });
   }
 
   async init(locator: ServiceLocator) {
@@ -75,6 +104,13 @@ export default class PixieService extends Service {
     server.on('connect', (...args) => this._on_connect(...args));
     server.on('disconnect', (...args) => this._on_disconnect(...args));
     server.on('login', (...args) => this._on_login(...args));
+  }
+
+  private async _recv(socket: Dealer) {
+    const [r] = await socket.receive();
+    const d: any = this.#decoder.decode(r);
+    this.#emitter.emit("data", d);
+    this.#recv_queue.add(() => this._recv(socket));
   }
 
   private _on_connect(endpoint: Endpoint) {
@@ -95,7 +131,7 @@ export default class PixieService extends Service {
 
   private _on_disconnect(endpoint: Endpoint) {
     if (this.#connected) {
-      this.#socket.disconnect(this.#endpoint);
+      this.#sockets.forEach((s) => s.disconnect(this.#endpoint));
     }
     this.#connected = false;
   }
@@ -150,15 +186,20 @@ export default class PixieService extends Service {
     if (!this.connected) {
       const e = endpoint ?? this.#endpoint;
       global.app.log.i('Connecting pixie to', e);
-      this.#socket.connect(e);
+      this.#sockets.forEach((s) => s.connect(e));
       this.#endpoint = e;
       global.app.log.i('Successfully connected pixie to', e);
       this.#connected = true;
     }
   }
 
+  private _generate_msg_id() {
+    return Math.floor(Math.random() * 10000000000);
+  }
+
   async plugin({ plugin_id }: { plugin_id: string }) {
     const r = await this.communicate({
+      msgid: this._generate_msg_id(),
       name: 'plugin_info',
       id: plugin_id,
     });
@@ -189,6 +230,7 @@ export default class PixieService extends Service {
     };
     if (i && it && s) {
       r = await this.communicate({
+        msgid: this._generate_msg_id(),
         name: 'image_generate',
         id: i,
         item_type: it,
@@ -197,12 +239,15 @@ export default class PixieService extends Service {
       });
     } else if (l1 && l2 && l3) {
       r = await this.communicate({
+        msgid: this._generate_msg_id(),
+
         name: 'image_link',
         link: [l1, l2, l3],
         type: t,
       });
     } else if (p1 && p2 && p3) {
       r = await this.communicate({
+        msgid: this._generate_msg_id(),
         name: 'image_path',
         link: [p1, p2, p3],
         type: t,
@@ -211,23 +256,66 @@ export default class PixieService extends Service {
     return r;
   }
 
-  async communicate(msg: unknown) {
-    if (!this.connected) {
-      throw Error('Pixie not connected');
-    }
+  communicate(msg: { msgid: any } & Record<string, any>) {
 
-    global.app.log.d('Sending pixie message', msg);
-    await this.#queue.add(async () => {
-      await this.#socket.send(this.#encoder.encode(msg));
-      global.app.log.d('Sent pixie message', msg);
-    });
-    const [r] = await this.#socket.receive();
-    const d: any = this.#decoder.decode(r);
-    if (d?.error) {
-      const e = Error(d.error);
-      e.code = d?.code;
-      throw e;
-    }
-    return d;
+    return new Promise((resolve, reject) => {
+
+      if (!this.connected) {
+        throw Error('Pixie not connected');
+      }
+
+      // global.app.log.d('Sending pixie message', msg);
+
+      let idx = 0;
+      let size = 0
+
+      this.#queues.forEach((q, i) => {
+        if (size === 0) {
+          size = q.size;
+          idx = i;
+        } else if (q.size < size) {
+          size = q.size;
+          idx = i;
+        }
+      });
+
+      const queue = this.#queues[idx];
+      const socket = this.#sockets[idx];
+
+      const cb = (data: {
+        msgid?: any,
+        error?: any,
+      }) => {
+        let e: any = undefined
+        if (data.msgid === msg.msgid) {
+          if (data?.error) {
+            e = Error(data.error);
+            e.code = data?.code;
+          } else {
+            this.#emitter.off("data", cb);
+            resolve(data);
+          }
+        } else if (!data.msgid) {
+          e = Error(data.error);
+          e.code = data?.code;
+        }
+
+        if (e) {
+          this.#emitter.off("data", cb);
+          reject(e);
+        }
+      }
+
+      this.#emitter.on("data", cb);
+
+      queue.add(async () => {
+        await socket.send(this.#encoder.encode(msg));
+      });
+
+      setTimeout(() => {
+        const e = new TimeoutError("Did not receive pixie response in time")
+        reject(e)
+      }, this.TIMEOUT)
+    })
   }
 }
